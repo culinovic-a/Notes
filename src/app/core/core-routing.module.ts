566c57394632e6ae230bb3e3b82f00ebf5337ae5
Aleksandra Culinovic <aleksandraculinovic@gmail.com>
2020-03-06 21:54:47 +0100

    set up firebase and social login

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LayoutComponent } from "./layout/layout.component";
+import { LoginComponent } from "./login/login.component";
 
-const routes: Routes = [{ path: "", component: LayoutComponent }];
+const routes: Routes = [
+  {
+    path: "",
+    component: LoginComponent
+  },
+  {
+    path: "layout",
+    component: LayoutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -8,9 +8,16 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatListModule } from "@angular/material/list";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { environment } from "../../environments/environment";
+import { AuthService } from "./services/auth.service";
+import { LoginComponent } from "./login/login.component";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, LoginComponent],
   imports: [
     CommonModule,
     FormsModule,
@@ -19,8 +26,16 @@ import { MatListModule } from "@angular/material/list";
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    AngularFireModule.initializeApp(
+      environment.firebase,
+      "angular-auth-firebase"
+    ),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    MatCardModule
   ],
+  providers: [AuthService],
   exports: [LayoutComponent]
 })
 export class CoreModule {}
