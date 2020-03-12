0581476dbe95981c648cab8c612265fcc9d57be2
Aleksandra Culinovic <aleksandraculinovic@gmail.com>
2020-03-12 21:55:35 +0100

    changed service methods

--- src/app/new-note/services/new-note.service.ts
+++ src/app/new-note/services/new-note.service.ts
@@ -1,25 +1,59 @@
 import { Injectable } from "@angular/core";
 import { Note } from "../models/note";
-import { AngularFirestore } from "@angular/fire/firestore";
+import {
+  AngularFirestore,
+  AngularFirestoreCollection
+} from "@angular/fire/firestore";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class NewNoteService {
-  constructor(private firestore: AngularFirestore) {}
+  notesCollection: AngularFirestoreCollection<Note>;
+  notes: Observable<Note[]>;
+
+  constructor(public firestore: AngularFirestore) {
+    this.notesCollection = this.firestore.collection("notes");
+
+    this.notes = this.notesCollection.snapshotChanges().map(changes => {
+      return changes.map(a => {
+        const data = a.payload.doc.data() as Note;
+        return data;
+      });
+    });
+  }
+
+  // public createNote(newNote: Note) {
+  //   this.firestore.collection("notes").add(newNote);
+  // }
 
   public createNote(newNote: Note) {
-    this.firestore.collection("notes").add(newNote);
+    return new Promise<any>((resolve, reject) => {
+      this.notesCollection.add(newNote).then(
+        res => {},
+        err => reject(err)
+      );
+    });
   }
 
-  getNotes() {
-    return this.firestore.collection("notes").snapshotChanges();
+  // getNotes(): Observable<Note> {
+  //   return this.firestore.collection("notes").snapshotChanges();
+  // }
+
+  public getNotes() {
+    return this.notes;
   }
 
-  getNote(id: string) {
-    return this.firestore
-      .collection("notes")
-      .doc(id)
-      .snapshotChanges();
+  // public getNote() {
+  //   return this.notesCollection
+  //     .doc()
+  //     .snapshotChanges();
+  // }
+
+  public getNote(id: string) {
+    return new Promise<any>((resolve, reject) => {
+      this.notesCollection.doc("notes/" + id).snapshotChanges();
+    });
   }
 }
