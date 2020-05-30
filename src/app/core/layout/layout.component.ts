5300e31c65952d0be5bdab4f77c13b68d5c31b1a
Aleksandra Culinovic <aleksandraculinovic@gmail.com>
2020-05-30 13:39:39 +0200

    created light theme and added functionality for changing dark-light mode

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-layout",
-  templateUrl: "./layout.component.html",
-  styleUrls: ["./layout.component.scss"]
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
 })
 export class LayoutComponent implements OnInit {
   constructor() {}
 
+  isLightTheme = false;
+
   ngOnInit() {}
+
+  changeTheme(): void {
+    if (this.isLightTheme) {
+      this.isLightTheme = false;
+    } else {
+      this.isLightTheme = true;
+    }
+  }
 }
