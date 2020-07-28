bc910786cf7608a7a6b0a8fa7524f84a24270585
Aleksandra Culinovic <aleksandraculinovic@gmail.com>
2020-07-28 13:28:46 +0200

    added functionality for saving theme

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -10,6 +10,8 @@ import { SnackService } from '../services/snack.service';
 export class LayoutComponent implements OnInit {
   isLightTheme = false;
   userIsLoggedIn = localStorage.getItem('userUid') != null ? true : false;
+  checkTheme: string;
+  toggleButton;
 
   constructor(
     private authService: AuthService,
@@ -18,13 +20,31 @@ export class LayoutComponent implements OnInit {
 
   ngOnInit() {
     this.checkIfUserIsLoggedIn();
+    this.checkThemeState();
+    this.toggleButton = JSON.parse(localStorage.getItem('toggleButtonState'));
+  }
+
+  checkThemeState(): void {
+    this.checkTheme = localStorage.getItem('theme');
+    if (this.checkTheme === 'light-theme') {
+      this.isLightTheme = true;
+      document.getElementById('themeTag').classList.add('light-theme');
+    }
   }
 
   changeTheme(): void {
     this.isLightTheme = !this.isLightTheme;
+
+    localStorage.setItem(
+      'toggleButtonState',
+      JSON.stringify(this.isLightTheme)
+    );
+
     if (this.isLightTheme) {
+      localStorage.setItem('theme', 'light-theme');
       document.getElementById('themeTag').classList.add('light-theme');
     } else {
+      localStorage.removeItem('theme');
       document.getElementById('themeTag').classList.remove('light-theme');
     }
   }
